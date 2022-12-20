7a581cd8ff237d2df10499304942c941c748586a
HanifNurHidayat <101539719+HanifNurHidayat@users.noreply.github.com>
2022-12-20 14:37:36 +0700

    Add files via upload

---
+++ laravel8/database/migrations/2022_10_18_014629_create_karyawan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable(false);
+            $table->string('email', 100)->nullable(false);
+            $table->string('telepon', 15)->nullable(false);
+            $table->text('alamat')->nullable(false);
+            $table->enum('jenis_kelamin', ['pria', 'wanita'])->nullable(false);
+            $table->string('tempat_lahir', 50)->nullable(false);
+            $table->date('tanggal_lahir')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+}
